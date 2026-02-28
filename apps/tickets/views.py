fd26b1c36c995da49937ebd4893d2d2739e4506f
Lameck Irungu <mugolameck@gmail.com>
2026-02-28 21:01:32 +0300

    Implement TicketsViewset with authentication and admin permissions

--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, views, generics, authentication
+from rest_framework import permissions
+from django.conf import settings
 
-# Create your views here.
+from .serializers import TicketSerializer
+from .models import Ticket
+
+User = settings.AUTH_USER_MODEL
+
+class TicketsViewset(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+       
+        
